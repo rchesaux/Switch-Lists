1701988b5504695f415f851b64d2833713688db7
rchesaux <chesaux66@gmail.com>
2024-10-30 10:30:52 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const changeList = () => {
+    const sweetIsChecked = document.getElementById('sweet').checked;
+    const savoryIsChecked = document.getElementById('savory').checked;
+
+    const sweetList = ["chocolate", "m&ms"]
+    const savoryList = ["bread", "burger"]
+
+    if (sweetIsChecked) {
+        let item1 = document.createElement('li');
+        let item1Text = document.createTextNode(sweetList[i])
+        new
+        
+    } else if (savoryIsChecked) {
+
+    } else {
+
+    }
+    
+}
